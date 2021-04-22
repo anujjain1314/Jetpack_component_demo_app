4683694af6be63e211dca22130a5ae49a6505caf
Anuj Jain <anuj@xoxoday.com>
2021-04-22 12:57:54 +0530

    added retry button funtionality and added some animations to make transitions smooth

--- app/src/main/java/com/anujjain/getMegaAssignment/errorStates/NetworkErrorFragment.kt
+++ app/src/main/java/com/anujjain/getMegaAssignment/errorStates/NetworkErrorFragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.anujjain.getMegaAssignment.ApiStatus
 import com.anujjain.getMegaAssignment.R
 import com.anujjain.getMegaAssignment.databinding.FragmentErrorStateBinding
 
@@ -18,6 +22,24 @@ class NetworkErrorFragment : Fragment(){
     ): View? {
         val binding : FragmentErrorStateBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_error_state,container,false)
 
+        val viewModel = ViewModelProvider(this).get(NetworkErrorViewModel::class.java)
+        // allows the xml file to access viewmodel object
+        binding.viewModel = viewModel
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+
+        /**
+         *
+         * observe the [viewModel.eventRetryAgain] and navigate back to [TrendingRepoFragment] and calls [viewModel.onClickRetryDone()]
+         */
+        viewModel.eventRetryAgain.observe(viewLifecycleOwner, Observer {retryAgain->
+            if(retryAgain){
+                findNavController().navigate(R.id.action_networkErrorFragment_to_trendingRepoFragment)
+                viewModel.onClickRetryDone()
+            }
+
+        })
         return binding.root
     }
 }
--- app/src/main/java/com/anujjain/getMegaAssignment/errorStates/NetworkErrorViewModel.kt
+++ app/src/main/java/com/anujjain/getMegaAssignment/errorStates/NetworkErrorViewModel.kt
@@ -1,7 +1,20 @@
 package com.anujjain.getMegaAssignment.errorStates
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NetworkErrorViewModel : ViewModel() {
+
+    private val _eventRetryAgain = MutableLiveData<Boolean>()
+    val eventRetryAgain : LiveData<Boolean> get() = _eventRetryAgain
+
+    fun onClickRetry(){
+        _eventRetryAgain.value = true
+    }
+
+    fun onClickRetryDone(){
+        _eventRetryAgain.value = false
+    }
 
 }
