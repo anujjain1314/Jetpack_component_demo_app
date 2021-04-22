224dd1f5d0332f8e12385d5c11982b22e9866781
Anuj Jain <anuj@xoxoday.com>
2021-04-22 16:26:54 +0530

    created database related objects to store data fetched from network

--- app/src/main/java/com/anujjain/getMegaAssignment/database/DatabaseEntities.kt
+++ app/src/main/java/com/anujjain/getMegaAssignment/database/DatabaseEntities.kt
@@ -1,13 +1,14 @@
-package com.anujjain.getMegaAssignment.network
+package com.anujjain.getMegaAssignment.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-
-
-
+@Entity(tableName = "trending_repo")
 data class TrendingRepoDataModel (
     val author : String,
     val name : String,
     val avatar : String,
+    @PrimaryKey
     val url : String,
     val description : String,
     val language : String,
@@ -28,4 +29,3 @@ data class BuiltBy (
     val avatar : String,
     val username : String
 )
-
---
+++ app/src/main/java/com/anujjain/getMegaAssignment/database/TrendingRepoDAO.kt
@@ -0,0 +1,22 @@
+package com.anujjain.getMegaAssignment.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+/**
+ * Defines methods for using the TrendingRepoDataModel
+ * and perform operations related to it
+ * on the [TrendingRepoDataBase].
+ */
+@Dao
+interface TrendingRepoDAO {
+    @Query("select * FROM trending_repo")
+    fun getRepos() : LiveData<List<TrendingRepoDataModel>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertRepoList(repoList : List<TrendingRepoDataModel>)
+}
