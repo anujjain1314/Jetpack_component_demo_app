47e9721f28536f8da5ab414e1597108d1ae0828c
Anuj Jain <anuj@xoxoday.com>
2021-04-22 18:31:53 +0530

    added comments and documentation for TrendingRepoRepository

--- app/src/main/java/com/anujjain/getMegaAssignment/repository/TrendingRepoRepository.kt
+++ app/src/main/java/com/anujjain/getMegaAssignment/repository/TrendingRepoRepository.kt
@@ -8,10 +8,27 @@ import com.anujjain.getMegaAssignment.network.RetrofitApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+/**
+ * This repository class is responsible for fetching Repo data from multiple data source
+ * i.e from [TrendingRepoDataBase] or from network
+ */
+
 class TrendingRepoRepository(private val dataBase: TrendingRepoDataBase) {
 
+
+    // provide the data stored in [TrendingRepoDataBase]
     val repoList : LiveData<List<TrendingRepoDataModel>> = dataBase.trendingRepoDAO.getRepos()
 
+
+    /**
+     * Forcefully fetch the data from network and
+     * Refresh the data in [TrendingRepoDataBase]
+     *
+     *
+     * This function uses the IO dispatcher to ensure the database insert database operation
+     * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext` this
+     * function is now safe to call from any thread including the Main thread.
+     */
     suspend fun refreshRepoList(){
         withContext(Dispatchers.IO){
             Log.e("TrendingRepoRepository", "refresh repo list called")
